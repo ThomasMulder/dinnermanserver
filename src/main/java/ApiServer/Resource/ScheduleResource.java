03e79daf4784c53cf5ccd4dbe6ad42510b041c30
LBuilder <lbuildermail@gmail.com>
2016-04-09 10:52:14 +0200

    TEMP IndexOutOfBounds bug fix for Schedule resource.

--- src/main/java/ApiServer/Resource/ScheduleResource.java
+++ src/main/java/ApiServer/Resource/ScheduleResource.java
@@ -28,8 +28,10 @@ public class ScheduleResource extends AbstractScheduleResource {
                 List<Integer> allowedByCuisine = getAllowedCuisineIds(account_id, cuisine);
                 List<Integer> allowedByAllergen = Database.getInstance().getAllowedRecipeIds(account_id);
                 List<Integer> allowedIds = getListIntegerIntersection(allowedByCuisine, allowedByAllergen);
-                int id = allowedIds.get(getRandomIndex(allowedIds));
-                schedule.add(getRecipeById(id));
+                if (!allowedIds.isEmpty()) {
+                    int id = allowedIds.get(getRandomIndex(allowedIds));
+                    schedule.add(getRecipeById(id));
+                }
             }
             this.returnResponse(response, schedule, new ScheduleSerializer());
         }
